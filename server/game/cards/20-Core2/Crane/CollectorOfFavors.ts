0f2bf4f74a7fa15f366748e2d712467066244299
Luiz Socrate <luiz@socrate.com.br>
2025-04-27 18:17:27 +0200

    Remove courtesy from Collector of Favors

--- server/game/cards/20-Core2/Crane/CollectorOfFavors.ts
+++ server/game/cards/20-Core2/Crane/CollectorOfFavors.ts
@@ -9,8 +9,6 @@ export default class CollectorOfFavors extends DrawCard {
     setupCardAbilities() {
         this.attachmentConditions({ trait: 'courtier' });
 
-        this.whileAttached({ effect: AbilityDsl.effects.addKeyword('courtesy') });
-
         this.whileAttached({
             effect: AbilityDsl.effects.gainAbility(AbilityTypes.Reaction, {
                 title: 'Gain 1 fate',
