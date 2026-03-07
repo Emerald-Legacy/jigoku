3fe13f4022a0b28f707bcef12690b41869b34218
Benjamin Wolf <benjamin.wolf.de@me.com>
2026-03-07 20:28:45 +0100

    Remove pmx deps, update uuid

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const Server = require('./server.js');
 const Lobby = require('./lobby.js');
-const pmx = require('pmx');
 const db = require('./db.js');
 const env = require('./env.js');
 
@@ -12,36 +11,6 @@ async function runServer() {
     server.initDb();
     var httpServer = server.init();
     var lobby = new Lobby(httpServer, { db: database });
-
-    pmx.action('status', (reply) => {
-        var status = lobby.getStatus();
-
-        reply(status);
-    });
-
-    pmx.action('disable', (param, reply) => {
-        if(!param) {
-            reply({ error: 'Need to specify node to disable' });
-
-            return;
-        }
-
-        reply({ success: lobby.disableNode(param) });
-    });
-
-    pmx.action('enable', (param, reply) => {
-        if(!param) {
-            reply({ error: 'Need to specify node to enable' });
-
-            return;
-        }
-
-        reply({ success: lobby.enableNode(param) });
-    });
-
-    pmx.action('debug', (reply) => {
-        reply(lobby.debugDump());
-    });
 
     server.run();
 }
